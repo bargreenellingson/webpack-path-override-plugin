7267bd0aa651e037e0f6ecdad7c995ce73ed1422
pcvonz@gmail.com <pzimmerman@bargreen.com>
2019-01-08 12:51:59 -0800

    Remove hard coded options

--- src/webpack-path-override-plugin.js
+++ src/webpack-path-override-plugin.js
@@ -7,13 +7,15 @@ var path = require('path');
 module.exports =  class WebpackPathOverridePlugin {
   constructor(mainDir, overrideDir){
     this.mainDir = mainDir;
+    this.overrides = []
     this.overrideDir = overrideDir;
-    this.walk(this.overrideDir);
   }
 
   apply(compiler) {
-    compiler.hooks.normalModuleFactory.tap('FileOverridePlugin', (fop) => {
-      fop.hooks.beforeResolve.tap('FileOverridePlugin', (result) => {
+   compiler.hooks.normalModuleFactory.tap('FileOverridePlugin', (fop) => {
+      this.options = compiler.options.resolve;
+      this.walk(this.overrideDir);
+       fop.hooks.beforeResolve.tap('FileOverridePlugin', (result) => {
         if (!result) return;
 
         let requestFullPath = path.resolve(result.context, result.request);
@@ -23,27 +25,29 @@ module.exports =  class WebpackPathOverridePlugin {
           let pathFromPivot = this.getRelativeFromRootDir(result);
           if (this.overrides.includes(pathFromPivot)) {
             result.request = path.resolve(this.overrideDir + '/..', pathFromPivot);
+            console.log(result.request);
           }
 
+          // return resolver.doResolve(target, request, null, resolveContext, callback);
           return result;
         } else {
+          // return resolver.doResolve(target, request, null, resolveContext, callback);
           return result;
         }
       });
     });
   }
 
-  getRelativeFromRootDir(result) {
+  getRelativeFromRootDir(request) {
     let subPath = '';
-    if (result.request.split(path.sep)[0] === 'src') {
-      return result.request;
+    let aliasKeys = Object.keys(this.options.alias);
+    if (aliasKeys.includes(request.request.split(path.sep)[0])) {
+      return request.request;
     } else {
-      subPath = path.relative(this.mainDir, result.context);
-      return path.join(subPath, result.request);
+      subPath = path.relative(this.mainDir, request.context);
+      return path.join(subPath, request.request);
     }
   }
-
-  overrides = [];
 
   walk = (directoryName) => {
     fs.readdir(directoryName, (e, files) => {
@@ -62,9 +66,12 @@ module.exports =  class WebpackPathOverridePlugin {
             this.walk(fullPath);
           } else {
             let relativePath = path.relative(this.overrideDir + '/..',fullPath)
-            if (path.extname(relativePath) === '.js') {
-              relativePath = path.dirname(relativePath) + '/' + path.basename(relativePath, '.js')
+
+            // Add file without extension if it's extensions is included in options
+            if (this.options.extensions.includes(path.extname(relativePath))) {
+              this.overrides.push(path.dirname(relativePath) + '/' + path.basename(relativePath, path.extname(relativePath)));
             }
+
             this.overrides.push(relativePath);
           }
         });
